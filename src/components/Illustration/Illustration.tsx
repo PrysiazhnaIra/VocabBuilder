d5794632d93480b3f9e89d7953d606a34b4f8d5b
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2025-10-13 16:46:56 +0300

    optimization pictures/support retina

--- src/components/Illustration/Illustration.tsx
+++ src/components/Illustration/Illustration.tsx
@@ -2,9 +2,14 @@ import s from "./Illustration.module.css";
 export default function Illustration() {
   return (
     <picture className={s.wrapper}>
-      <source srcSet="/images/people_desk.png" media="(min-width:1440px)" />
+      <source
+        srcSet="/images/people_desk.png 1x,
+                /images/people_desk_2x.png 2x"
+        media="(min-width:1440px)"
+      />
       <img
         src="/images/people_mob.png"
+        srcSet="/images/people_mob_2x.png 2x"
         alt="A man and a woman sitting and reading a book, symbolizing learning."
         className={s.image}
       />
