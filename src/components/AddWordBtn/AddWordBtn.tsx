cebade170340075a030b6e6f33f6cb5f125b9967
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2025-11-24 22:13:06 +0200

    adding word

--- src/components/AddWordBtn/AddWordBtn.tsx
+++ src/components/AddWordBtn/AddWordBtn.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import Button from "../Button/Button";
 import Icon from "../Icon/Icon";
 import s from "./AddWordBtn.module.css";
+import AddWordModal from "../AddWordModal/AddWordModal";
 
 export default function AddWordBtn() {
+  const [isAddWordModalOpen, setIsAddWordModalOpen] = useState(false);
+
   return (
     <Button className={s.addWordBtn}>
-      <div className={s.addWordBtnWrapper}>
+      <div
+        className={s.addWordBtnWrapper}
+        onClick={() => setIsAddWordModalOpen(true)}
+      >
         <p>Add word</p>
         <Icon name="icon-plus" className={s.icon} />
       </div>
+
+      {isAddWordModalOpen && (
+        <AddWordModal onClose={() => setIsAddWordModalOpen(false)} />
+      )}
     </Button>
   );
 }
